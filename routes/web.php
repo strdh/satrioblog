38f89e11b132d470349bd0efb635d39576bd5bc7
strdh <strdh33@gmail.com>
2021-12-31 00:46:08 +0700

    Update

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontPage\FrontPageController;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use App\Http\Controllers\Management\ManagementController;
 
 Route::get('/', [FrontPageController::class, 'index'])->name('frontpage.index');
 Route::get('/allpost', [FrontPageController::class, 'posts'])->name('frontpage.allpost');
 Route::get('/post/{post:slug}', [FrontPageController::class, 'post'])->name('frontpage.post');
 Route::get('/allcategories', [FrontPageController::class, 'categories'])->name('frontpage.allcategory');
 Route::get('/post/categories/{categories:slug}', [FrontPageController::class, 'postByCategory'])->name('frontpage.by_category');
+
+Route::prefix('management')
+		->as('management.')
+		->group(function() {
+            Route::get('/', [ManagementController::class, 'index'])->name('index');
+        });
